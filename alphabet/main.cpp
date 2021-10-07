aa1cbe89f1ac7f5b43e7cc7bf81a47ab71a57a7c
Ермолаева Настя <ir.ermolaeva@gmail.com>
2021-10-07 16:01:01 +0300

    added b

--- alphabet/main.cpp
+++ alphabet/main.cpp
@@ -10,12 +10,12 @@ private:
 public:
     symbol(string i1, string i2, string i3, string i4, string i5, string i6)
     {
-        graphics[0]=i1;
-        graphics[1]=i2;
-        graphics[2]=i3;
-        graphics[3]=i4;
-        graphics[4]=i5;
-        graphics[5]=i6;
+        graphics[0] = i1;
+        graphics[1] = i2;
+        graphics[2] = i3;
+        graphics[3] = i4;
+        graphics[4] = i5;
+        graphics[5] = i6;
     }
     string getline(int i)
     {
@@ -30,18 +30,24 @@ private:
 public:
     void add(symbol toadd)
     {
-        for(int i=0; i<6; i++)
+        for (int i(0); i<6; i++)
             graphics[i]+=toadd.getline(i);
     }
     void print()
     {
-        for(auto i: graphics)
-            cout << i << endl;
+    for (auto i: graphics)
+        cout << i << endl;
     }
 };
+
 screen mainscreen;
+
 int main()
 {
+    symbol b( "|-----| " ,  "|     | " ,  "|_____  " ,  "|     | " ,  "|     | " ,  "|_____| ");
+
+    mainscreen.add(b);
+
     mainscreen.print();
     return 0;
 }
